9c5cc6c7923e7a3ffbbfcea1aa84bc17085d992d
Konstantin <games2010@mail.ru>
2017-04-02 23:51:53 +0300

    FirstCommit

---
+++ cms/index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+echo "2gg";
+class good { 
+    private $good_id;
+    private $good_cid;
+
+
+    private $good_title;
+
+    public function setGoodTitle($good_title) {
+        $this->good_title = $good_title;
+    }
+}
+
+
+echo "dwqdq";
